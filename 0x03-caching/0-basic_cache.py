9816de3fa5b3c33b60ccdf524555fb03e2e20808
Rémi Marçais <remi.marcais87@gmail.com>
2023-01-26 13:12:59 +0100

    Added all files from task 0

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        return self.cache_data[key]
