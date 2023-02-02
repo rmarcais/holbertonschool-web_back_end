7bd2dfa1136930f9f96e9fd09eddcfbc0f645ca8
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-02 12:50:28 +0100

    Added is_valid to encrypt_password module

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     hash = bcrypt.hashpw(bytes, salt)
 
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password"""
+    bytes = password.encode("utf-8")
+
+    return bcrypt.checkpw(bytes, hashed_password)
