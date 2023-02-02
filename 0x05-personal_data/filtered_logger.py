229100a44f3f5c5aafccd09c630c61e53fb7c18b
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-02 12:11:11 +0100

    Tried to bugs

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -14,7 +14,7 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log messafe obfuscated"""
     for field in fields:
-        pattern = r"(?<={}=)[^;]*(?={})".format(field, separator)
+        pattern = "(?<={}=)[^;]*(?={})".format(field, separator)
         message = re.sub(pattern, redaction, message)
     return message
 
