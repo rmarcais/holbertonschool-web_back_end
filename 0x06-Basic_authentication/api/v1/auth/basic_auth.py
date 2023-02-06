2dd9cfdbb2e8d1a4c2b6210e5de7c2d71a06b2cb
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-06 16:55:58 +0100

    Improved basic_auth.py

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic_auth module containing the BasicAuth class """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str)\
+            -> str:
+        """ Returns the decoded value of base64_authorization_header """
+        if (base64_authorization_header is None or
+                type(base64_authorization_header) is not str):
+            return None
+
+        try:
+            return (base64.b64decode(base64_authorization_header)
+                          .decode('utf-8'))
+        except Exception:
+            return None
