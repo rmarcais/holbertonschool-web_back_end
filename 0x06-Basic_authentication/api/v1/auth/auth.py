d3574a7494ebd44d14c5ce77258b0d92a03736e6
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-06 14:43:31 +0100

    Improved the method require_auth in Auth class

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,22 @@ from typing import List, TypeVar
 class Auth():
     """ Auth class """
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Return False """
-        return False
-
+        if not path or not excluded_paths or excluded_paths == []:
+            return True
+        if path[-1] == "/":
+            path = path[:-1]
+        for i in range(len(excluded_paths)):
+            if excluded_paths[i][-1] == "/":
+                excluded_paths[i] = excluded_paths[i][:-1]
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Return None """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retutn None """
