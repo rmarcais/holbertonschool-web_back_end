b3744ce7836e8054f3fd23d967943bf287197567
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-08 14:41:01 +0100

    Updated the @app.before_request method in app.py

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -32,9 +32,11 @@ def before_request():
         require_auth = auth.require_auth(request.path,
                                          ['/api/v1/status/',
                                           '/api/v1/unauthorized/',
-                                          '/api/v1/forbidden/'])
+                                          '/api/v1/forbidden/',
+                                          '/api/v1/auth_session/login/'])
         if require_auth is True:
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
                 abort(401)
             request.current_user = auth.current_user(request)
             if request.current_user is None:
