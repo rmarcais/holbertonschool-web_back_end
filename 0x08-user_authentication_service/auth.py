f809b6db0a427e5fbea311502c0f4817467feaea
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-13 16:55:39 +0100

    Added the Auth.get_user_from_session_id medthod

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -70,3 +70,14 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> TypeVar("User"):
+        """Returns the corresponding User or None"""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
