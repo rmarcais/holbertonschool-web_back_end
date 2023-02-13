f5f5fcf5a8cdf04969b3bb2d5d10a716fc4c0ec8
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-13 15:25:00 +0100

    Implemented the end-point to register a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,15 +2,31 @@
 """Route module for the API
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
 
+auth = Auth()
+
 
 @app.route('/')
-def welcome():
+def welcome() -> str:
     """Says weclome !"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """End-point to register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        auth.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "{}".format(email), "message": "user created"})
 
 
 if __name__ == "__main__":
