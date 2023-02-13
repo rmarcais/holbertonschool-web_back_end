5d9033b3890d31b99a460932de005222baa121f7
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-13 13:23:06 +0100

    Added the DB.find_user_by method

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -17,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -43,3 +45,17 @@ class DB:
         session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> TypeVar("User"):
+        """
+        Returns the first row found in the users table
+        as filtered by the arguments kwargs
+        """
+        session = self._session
+        try:
+            query = session.query(User).filter_by(**kwargs)
+            if not query.first():
+                raise NoResultFound
+        except InvalidRequestError:
+            raise
+        return query.first()
