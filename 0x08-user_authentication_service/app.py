d409c26d82fbdc7663074c7f0e3522e185931ac8
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-14 10:16:33 +0100

    Added logout function

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -44,5 +44,16 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """logout function"""
+    session_id = request.cookies.get("session_id")
+    try:
+        user = auth.get_user_from_session_id(session_id)
+        auth.destroy_session(user.id)
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
