a392b03385fdcfa7761852c35cd2bede0f49f50c
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-20 15:52:45 +0100

    Defined the TestGetJson(unittest.TestCase) class and implemented the TestGetJson.test_get_json method

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
+from unittest.mock import patch, Mock
 import unittest
 
 
@@ -15,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test the method access_nested_map"""
+        """Tests the access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -23,7 +25,30 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), "'b'")
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
-        """Test the method access_nested_map"""
+        """Tests the access_nested_map method"""
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(expected, str(error.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Tests the get_json method"""
+        patcher = patch("requests.get")
+        mock_get = patcher.start()
+
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+
+        self.assertIsInstance(mock_get.return_value, Mock)
+        self.assertEqual(get_json(test_url), test_payload)
+
+        patcher.stop()
