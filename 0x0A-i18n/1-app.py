90cb448364ee58e9984b3d31e6b6b7f7c74efe66
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-23 13:43:09 +0100

    Added all files from task 3

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -21,7 +21,7 @@ app.config.from_object(Config)
 @app.route("/")
 def hello_world():
     """Route that renders a simple template"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Route module for the API"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 from typing import List
 
 app = Flask(__name__)
@@ -31,7 +31,7 @@ def get_locale() -> List:
 @app.route("/")
 def hello_world():
     """Route that renders a simple template"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
