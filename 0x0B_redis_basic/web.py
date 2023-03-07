2d0b9be726f27220d1e64b619d33af65412a30d2
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-07 14:54:45 +0100

    Add web module

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Web module"""
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+
+def count_requests(f: Callable) -> Callable:
+    """
+    System to count how many times methods
+    of the Cache class are called
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        """
+        Increments the count for the key key
+        every time the method method is called
+        """
+        key = "count:{}".format(*args)
+        local_redis = redis.Redis()
+        local_redis.incr(key)
+        local_redis.expire(key, 10)
+
+        return f(*args)
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """Returns the HTML content of a particular URL"""
+    r = requests.get(url)
+
+    return r.text
