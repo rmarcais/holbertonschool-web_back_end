030f86f297ce1117d0ad732ac0b977045042eeeb
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-07 10:50:35 +0100

    Redefine get method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Exercise module"""
 
 import redis
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 
 
@@ -20,3 +20,26 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int, float, None]:
+        """Redifines the Redis.get() method"""
+        value = self._redis.get(key)
+        if not value:
+            return None
+
+        if fn == int:
+            value = self.get_int(value)
+        elif fn:
+            value = self.get_str(value)
+
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """Returns data as a string"""
+        return data.decode("utf-8")
+
+    def get_int(self, data: bytes) -> int:
+        """Returns data as an integer"""
+        return int(data)
