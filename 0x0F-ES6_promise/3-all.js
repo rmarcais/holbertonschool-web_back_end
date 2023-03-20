ca0d779714b66f1318aea92a316c712f1e550390
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-20 11:42:55 +0100

    Add 3-all.js

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then(
+    (value) => console.log(value[0].body, value[1].firstName, value[1].lastName),
+  ).catch('Signup system offline');
+}
