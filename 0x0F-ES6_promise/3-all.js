dcb9f08ef7f02ae5ef23dfd188c3dd757e2c650f
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-21 00:09:10 +0100

    Fix issue when promise is rejected

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(
     (value) => console.log(value[0].body, value[1].firstName, value[1].lastName),
-  ).catch('Signup system offline');
+  ).catch(() => console.log('Signup system offline'));
 }
