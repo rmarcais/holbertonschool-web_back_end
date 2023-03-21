6dbfcd64dda276e3b6f174a1c8aacacdb9005c6f
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-21 01:35:52 +0100

    Improve 6-final-user.js

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,6 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
-    (value) => value.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : result.reason.message })),
+    (value) => value.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}` })),
   ));
 }
