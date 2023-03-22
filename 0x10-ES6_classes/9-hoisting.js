8f4a5118e941efce9c34bc8d18e2d8ffdf197ee7
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-23 00:17:37 +0100

    Put the two classes to the top

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -13,9 +13,6 @@ class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -35,6 +32,9 @@ class StudentHolberton {
     return `${this.fullName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
