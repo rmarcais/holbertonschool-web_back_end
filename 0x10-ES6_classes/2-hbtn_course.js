cda6afea0d4116c6ebfd8ce3d15b981d35ef767d
Rémi Marçais <remi.marcais87@gmail.com>
2023-03-22 14:14:24 +0100

    Add 2-hbtn_course.js

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!(students instanceof Array)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!(students instanceof Array)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+}
