695d570abbb0904b77a82c789cb25e194f9850a7
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-01 13:23:24 +0200

    Add module.exports

--- 0x12-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x12-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,6 @@
-export default class AppController {
+module.exports = class AppController {
   static getHomepage(request, response) {
     response.status(200);
     response.send('Hello Holberton School!');
   }
-}
+};
