7b5593083a9f45ec223dd6f4a2448e49367f9cef
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-01 12:06:48 +0200

    Go back to the previous version

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,8 +4,8 @@ const DB = process.argv[2];
 
 export default class StudentsController {
   static getAllStudents(request, response) {
-    response.write('This is the list of our students');
     readDatabase(DB).then((result) => {
+      response.write('This is the list of our students');
       Object.keys(result).sort().forEach((key) => {
         response.write(`\nNumber of students in ${key}: ${result[key].length}. List: ${result[key].join(', ')}`);
       });
