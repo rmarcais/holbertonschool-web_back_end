4d1ccbc870dfd8cb7131806ec078037ad8ae11e8
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-04 16:22:32 +0200

    Add all files from task 4

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -4,14 +4,17 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
     it('Validates the usage of the Utils function', function() {
-        const calculate = sinon.spy(Utils, 'calculateNumber');
+        const calculate = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const log = sinon.spy(console, 'log');
         const totalAmount = 100;
         const totalShipping = 20
 
         sendPaymentRequestToApi(totalAmount, totalShipping);
 
         calculate.restore();
+        log.restore();
         sinon.assert.calledOnce(calculate);
         sinon.assert.calledWith(calculate, 'SUM', totalAmount, totalShipping);
+        sinon.assert.calledWith(log, 'The total is: 10');
     });
 });
