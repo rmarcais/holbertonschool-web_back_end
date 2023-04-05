3b23d124ea24c33e9c9dbb9392281b6e6a8b3daa
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-05 12:42:27 +0200

    Try to fix bug

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -2,8 +2,9 @@ module.exports = function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   } else if (type === 'SUBSTRACT') {
-      return Math.round(a) - Math.round(b);
+    return Math.round(a) - Math.round(b);
   } else if (type === 'DIVIDE') {
-      return Math.round(b) !== 0 ? Math.round(a) / Math.round(b): 'Error';
+    return Math.round(b) !== 0 ? Math.round(a) / Math.round(b): 'Error';
   }
+  return 'Error';
 };
