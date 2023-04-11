e950d1805f81e9a4521fc212ef5eaec72e6b88cd
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-11 13:36:59 +0200

    Update 0-redis_client.js

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -4,4 +4,4 @@ const client = createClient();
 
 client.on('error', (error) => console.log('Redis client not connected to the server: ' + error.toString()));
 
-client.on('ready', () => console.log('Redis client connected to the server'));
+client.on('connect', () => console.log('Redis client connected to the server'));
