e86fdaee9e53071855662f7524f818041f5fb58f
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-12 13:23:32 +0200

    Add 6-job_creator.js

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const data = {
+  phoneNumber: '0612141617',
+  message: 'Hello World !',
+}
+
+const job = queue.create('push_notification_code', data).save((error) => {
+  console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('complete', () => {
+  console.log('Notification job failed');
+});
